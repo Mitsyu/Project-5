d863740e3ae59647d7d83f7a260ce39efa9a67cd
Jila Phonprasith <jilaphonprasith@gmail.com>
2023-03-20 10:41:26 -0400

    monday

--- Project 5/Program.cs
+++ Project 5/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Project_5
+﻿using System;
+
+namespace Project_5
 {
     internal class Program
     {
@@ -8,13 +10,11 @@
 
             while (running)
             {
-                Console.WriteLine("Main Menu");
-                Console.WriteLine("1.");
+                Console.WriteLine("--- Main Menu ---");
+                Console.WriteLine("1. Bank Program ");
                 Console.WriteLine("2.");
                 Console.WriteLine("3.");
                 Console.WriteLine("");
-
-
                 Console.WriteLine("Choose what program to run (1-): ");
                 string choice = Console.ReadLine(); 
 
@@ -26,26 +26,79 @@
                         break;
                         
                 }
-                Console.WriteLine();
-
             }
-
-            
+     
         }
         private static void BankProgram()
         {
             Random rnd = new Random();
             double bankAccount = rnd.Next(100, 151);
 
-            Console.WriteLine("wel");
-            Console.WriteLine("Current balance in your bank account: ${0}", bankAccount );
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("-- WELCOME TO THE BANK --");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Current balance in your bank account: ${0}", bankAccount);
+            Console.ResetColor();
             Console.WriteLine();
 
             Console.WriteLine("Enter transaction type (deposit, withdrawal, bill payment, or balance update): ");
             string transactionType = Console.ReadLine();
+            Console.WriteLine();
 
             Console.WriteLine("Enter transaction amount: ");
             double transactionAmount = double.Parse(Console.ReadLine());
+
+            switch(transactionType)
+            {
+                case "deposit":
+                    bankAccount += transactionAmount;
+                    break;
+                    
+                    
+                case "withdrawal":
+                    if (transactionAmount + 0.75 <= bankAccount)
+                    {
+                        bankAccount -= transactionAmount + 0.75;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Not enough funds");
+                        Console.ResetColor();
+                    }
+                    break;
+
+                case "bill payment":
+                    if (transactionAmount + 0.75 <= bankAccount)
+                    {
+                        bankAccount -= transactionAmount + 0.75;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Not enough funds");
+                        Console.ResetColor();
+                    }
+                    break;
+
+                case "balance update":
+                    bankAccount = transactionAmount + 0.75;
+                    break;
+
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid transaction type");
+                    Console.ResetColor();
+                    break;
+
+            }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("You now have ${0} in your your bank account.", bankAccount);
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine();
 
 
 
